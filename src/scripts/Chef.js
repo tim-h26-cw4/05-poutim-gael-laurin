dd3a5adb8b1df1cec58936139347949ae3a133d2
VirtualMan52 <virtu@virtualman52.com>
2026-02-26 14:01:07 -0500

    Créations méthodes et classes css

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -11,10 +11,18 @@ export default class Chef {
 
   init() {
     // Crée les instances de poutine
-    const poutines = this.element.querySelectorAll('.poutine');
+    const poutines = this.element.querySelectorAll('.js-poutine');
     for (let i = 0; i < poutines.length; i++) {
       const poutine = poutines[i];
       this.menu.push(new Poutine(poutine));
     }
+
+    // Écouteur de clic sur le bouton de commande
+    const bouton = this.element.querySelector('.js-order');
+    bouton.addEventListener('click', this.sendOrder.bind(this));
+  }
+
+  sendOrder() {
+    console.log('order sent!');
   }
 }
--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,10 +1,19 @@
 export default class Poutine {
   constructor(element) {
     this.element = element;
+    this.types = this.element.querySelectorAll('.js-button');
+    this.selectedType = '';
     this.init();
   }
 
   init() {
-    this.element.style.backgroundColor = 'red';
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.addEventListener('click', this.selectType.bind(this));
+    }
+  }
+
+  selectType(event) {
+    console.log(event.currentTarget.dataset.type);
   }
 }
