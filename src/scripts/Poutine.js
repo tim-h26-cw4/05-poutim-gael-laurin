2076640d7a2bb8c9e4644d9d2c56c3a7b826a1ab
VirtualMan52 <virtu@virtualman52.com>
2026-02-26 14:21:41 -0500

    Bouton désactivable

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -15,24 +15,42 @@ export default class Poutine {
   }
 
   selectType(event) {
-    // Change l'état actif des boutons
-    for (let i = 0; i < this.types.length; i++) {
-      const type = this.types[i];
-      type.classList.remove('is-active');
-    }
-    event.currentTarget.classList.add('is-active');
+    const buttonType = event.currentTarget.dataset.type;
 
-    // Change le type de poutine sélectionné
-    this.selectedType = event.currentTarget.dataset.type;
+    if (buttonType == this.selectedType) {
+      // Si ce type de poutine est déjà sélectionné,
+      // Désactive l'état actif du bouton
+      event.currentTarget.classList.remove('is-active');
+
+      // Enlève le type de poutine sélectionné
+      this.selectedType = '';
+    } else {
+      // Change l'état actif des boutons
+      for (let i = 0; i < this.types.length; i++) {
+        const type = this.types[i];
+        type.classList.remove('is-active');
+      }
+      event.currentTarget.classList.add('is-active');
+
+      // Change le type de poutine sélectionné
+      this.selectedType = buttonType;
+    }
 
     // Met à jour l'image de poutine
     this.updatePhoto();
   }
 
   updatePhoto() {
-    // Met à jour l'image de poutine selon le type sélectionné
     const image = this.element.querySelector('.js-image');
-    image.classList.add('is-active');
-    image.src = `assets/images/${this.selectedType}.png`;
+
+    if (this.selectedType == '') {
+      // Pas de poutine sélectionnée
+      image.classList.remove('is-active');
+      image.src = 'assets/images/poutine.png';
+    } else {
+      // Met à jour l'image de poutine selon le type sélectionné
+      image.classList.add('is-active');
+      image.src = `assets/images/${this.selectedType}.png`;
+    }
   }
 }
